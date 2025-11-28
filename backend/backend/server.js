fc63ce4f51a110e16debee0941a0e20e6d64c85a
jerryllorenz90-dot <jerryllorenz90@gmail.com>
2025-11-28 12:59:07 -0500

    Convert import statements to require syntax
    Replaced ES6 import statements with CommonJS require.

--- backend/backend/server.js
+++ backend/backend/server.js
@@ -1,13 +1,7 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-import statusRoute from "./routes/status.js";
-import pricesRoute from "./routes/prices.js";
-import historyRoute from "./routes/history.js";
-
-dotenv.config();
-
+const express = require("express");
+const cors = require("cors");
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
