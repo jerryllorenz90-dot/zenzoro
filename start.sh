3a10f177f564e10812a47c6687fe32c94c184432
jerryllorenz90-dot <jerryllorenz90@gmail.com>
2025-11-28 01:00:30 -0500

    Delete start.sh

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# Start backend and optionally serve frontend build
-cd backend
-node server.js &
-cd ..
-# If there's a client preview available, Replit will serve client via Vite preview if used
-wait
