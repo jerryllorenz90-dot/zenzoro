97e646d0517ce9d3ca6e98bce1fd29db9803b4c2
jerryllorenz90-dot <jerryllorenz90@gmail.com>
2025-12-05 19:06:32 -0600

    Delete prices.js

--- backend/routes/prices.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getPrice } = require("../controllers/priceController");
-
-router.get("/", getPrice);
-
-module.exports = router;
