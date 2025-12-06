7c38799a7b07f423ab98bdb9d9a72f82c0dfb4df
jerryllorenz90-dot <jerryllorenz90@gmail.com>
2025-12-05 19:09:21 -0600

    Update fetchRoutes.js

--- backend/routes/fetchRoutes.js
+++ backend/routes/fetchRoutes.js
@@ -1,10 +1,7 @@
-const fetchService = require("../services/fetchService");
+const express = require("express");
+const router = express.Router();
+const { getData } = require("../controllers/fetchController");
 
-exports.getData = async (req, res) => {
-  try {
-    const data = await fetchService.fetchData(req.query.endpoint);
-    res.json(data);
-  } catch (error) {
-    res.status(500).json({ error: "Failed to fetch data" });
-  }
-};
+router.get("/", getData);
+
+module.exports = router;
